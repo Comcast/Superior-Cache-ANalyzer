b53a5b0acced57d0064146d877c861d177dcac5e
ocket8888 <ocket8888@gmail.com>
2018-06-05 11:40:09 -0600

    TravisCI build script no longer prints unescaped linter output on lint failures

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ lintResults="$(pylint --rcfile='./.pylintrc' scan/ 2>/dev/null)"
 score=$(echo "$lintResults" | grep 'Your code has been rated at' | sed -r 's/[ a-zA-Z]//g' | cut -d '/' -f1)
 
 if [[ -z "$score" || $(echo "$score<9.50" | bc -l) -ne 0  ]]; then
-	echo $lintResults >&2
+	echo "$lintResults" >&2
 	exit 1
 fi
 
