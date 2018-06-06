d0e45b4dd8d02aa82790045697e1424a65b0353e
ocket8888 <ocket8888@gmail.com>
2018-06-06 10:50:50 -0600

    Starting to look at Autest for SCAN unit testing...

--- test.sh
+++ test.sh
@@ -10,10 +10,73 @@ fi
 
 echo "$lintResults"
 
-tests/test.py
+if [[ ! -z "$(which git 2>/dev/null)" && ! -z "$(which cc 2>/dev/null)" && ! -z "$(which autoreconf 2>/dev/null)" && ! -z "$(which make 2>/dev/null)" ]]; then
+	if [[ -d ats_test ]]; then
+		echo "Warning! removing existing 'ats_test' directory..." >&2
+		sleep 3 # Plenty of time to hit ^C
+		rm -rf ats_test
+	fi
 
-if [[ $? -ne 0 ]]; then
-	exit 1
+	git clone https://github.com/apache/trafficserver.git ats_test
+	pushd >/dev/null ats_test
+
+	if autoreconf -if ; then
+		#Nothing
+	else
+		echo "'autoreconf' has failed." >&2
+		exit 2
+	fi
+
+	mkdir goal
+
+	if ./configure --prefix "$(pwd)/goal"; then
+		#Nothing
+	else
+		echo "'./configure' has failed." >&2
+		exit 2
+	fi
+
+	if make -j; then
+		#Nothing
+	else
+		echo "'make' has failed." >&2
+	fi
+
+	if make install; then
+		#Nothing
+	else
+		echo "'make install' has failed." >&2
+	fi
+
+	for i in $(ls -A); do
+		case $i in
+			goal | tests )
+				;;
+			* )
+				rm -rf "$i";
+				;;
+		esac
+	done
+
+	mkdir -p "tests/gold_tests/scan/gold"
+
+	cp "../tests/scan.test.py" "tests/gold_tests/scan/"
+	cp "../tests/cache_populated.gold" "tests/gold_tests/scan/gold/"
+
+	if tests/autest.sh -f scan; then
+		#Nothing
+	else
+		echo "Autests failed..." >&2
+		exit 2
+	fi
+
+else
+	echo "Cannot run autests, need git, make, autoconf and a C compiler (and ATS dependencies)" >&2
 fi
 
-exit 0
+# tests/test.py
+
+# if [[ $? -ne 0 ]]; then
+# 	exit 1
+# fi
+
---
+++ tests/scan.test.py
@@ -0,0 +1,66 @@
+'''
+AuTest-based testing for the Superior Cache ANalyzer
+'''
+#  Licensed to the Apache Software Foundation (ASF) under one
+#  or more contributor license agreements.  See the NOTICE file
+#  distributed with this work for additional information
+#  regarding copyright ownership.  The ASF licenses this file
+#  to you under the Apache License, Version 2.0 (the
+#  "License"); you may not use this file except in compliance
+#  with the License.  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+import os
+Test.Summary = 'Test The Superior Cache ANalyzer'
+
+
+# Needs Curl
+Test.SkipUnless(
+    Condition.HasProgram("curl", "curl needs to be installed on system for this test to work")
+)
+Test.ContinueOnFail = False
+
+# Define default ATS
+ts = Test.MakeATSProcess("ts")
+server = Test.MakeOriginServer("server")
+
+#**testname is required**
+testName = ""
+request_header = {"headers": "GET / HTTP/1.1\r\nHost: www.example.com\r\n\r\n", "timestamp": "1469733493.993", "body": ""}
+response_header = {"headers": "HTTP/1.1 200 OK\r\nConnection: close\r\nCache-Control: public\r\n\r\n", "timestamp": "1469733493.993", "body": "yabadabadoo"}
+server.addResponse("sessionlog.json", request_header, response_header)
+
+# ATS Configuration
+ts.Disk.plugin_config.AddLine('xdebug.so')
+ts.Disk.records_config.update({
+    'proxy.config.diags.debug.enabled': 1,
+    'proxy.config.diags.debug.tags': 'http',
+    'proxy.config.http.response_via_str': 3,
+    'proxy.config.http.cache.http': 1,
+    'proxy.config.http.wait_for_cache': 1,
+})
+
+ts.Disk.remap_config.AddLine(
+    'map / http://127.0.0.1:{0}'.format(server.Variables.Port)
+)
+
+# Test 1 - 200 response and cache fill
+tr = Test.AddTestRun()
+tr.Processes.Default.StartBefore(server)
+tr.Processes.Default.StartBefore(Test.Processes.ts, ready=1)
+tr.Processes.Default.Command = 'curl -s -D - -v --ipv4 --http1.1 -H "x-debug: x-cache,via" -H "Host: www.example.com" http://localhost:{port}/'.format(port=ts.Variables.port)
+tr.Processes.Default.ReturnCode = 0
+tr.Processes.Default.Streams.stdout = "gold/cache_populated.gold"
+tr.StillRunningAfter = ts
+
+# At this point, there should be something(TM) in the cache...
+
+from scan import utils, directory, span, config, stripe, ui
+
